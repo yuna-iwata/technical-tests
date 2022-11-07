5e6dbd9532397cd2c82db3de0101197ca46fa3a3
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2022-11-07 11:49:00 +0000

    plan

---
+++ scrabble/scrabble.py
@@ -0,0 +1,13 @@
+# started 11:40 -> 13:10
+# plan 
+# functions: 
+# points() -> calc point for a letter
+# pointsforword() -> calc points for word using points()
+# drawseven() -> shuffle and draw seven tiles at random from bag
+# findvalidword() -> using the tiles from drawseven() find a word from dictionary.txt
+# longestvalidword() -> find the longest valid word  from findvalidword()
+# plan the rest if i get that far
+
+
+
+
